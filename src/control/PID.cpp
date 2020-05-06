4be7dfca6d2ac098f4c67d2e032671d0f275a5bc
FireAngel27 <peter.skuhra@gmail.com>
2020-05-06 23:32:04 +0200

    PID refactor

--- src/control/PID.cpp
+++ src/control/PID.cpp
@@ -13,25 +13,22 @@ control::PID::PID(float p_gain, float i_gain, float d_gain, float out_limit) :
 float control::PID::Update(float setpoint, float process)
 {
     float pid_output;
-    const uint16_t kSampleTime = 10;     // 10 ms
     const float kError = process - setpoint;
     const float kPrevError = prev_process_ - prev_setpoint_;
 
     uint32_t current_time = millis();
     uint32_t elapsed_time = current_time - prev_time_;
 
-    if (elapsed_time >= kSampleTime) {
-        outputs_.p = kError * gains_.p;
-        // outputs_.i = (outputs_.i + (kError * elapsed_time)) * gains_.i;
-        outputs_.i += (kError * gains_.i);
-        // outputs_.d = ((kError - kPrevError) / elapsed_time) * gains_.d;
-        outputs_.d = (kError - kPrevError) * gains_.d;
+    outputs_.p = kError * gains_.p;
+    outputs_.i = (outputs_.i + (kError * elapsed_time)) * gains_.i;
+    // outputs_.i += (kError * gains_.i);
+    outputs_.d = ((kError - kPrevError) / elapsed_time) * gains_.d;
+    // outputs_.d = (kError - kPrevError) * gains_.d;
 
-        prev_setpoint_ = setpoint;
-        prev_process_ = process;
+    prev_setpoint_ = setpoint;
+    prev_process_ = process;
 
-        pid_output = outputs_.p + outputs_.i + outputs_.d;
-    }
+    pid_output = outputs_.p + outputs_.i + outputs_.d;
 
     prev_time_ = current_time;
 
