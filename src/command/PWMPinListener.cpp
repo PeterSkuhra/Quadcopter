70f9244c933da6a542397fa711d6b61f3c92fd47
FireAngel27 <peter.skuhra@gmail.com>
2020-05-18 20:45:14 +0200

    HandleInterrupt optimization

--- src/command/PWMPinListener.cpp
+++ src/command/PWMPinListener.cpp
@@ -88,70 +88,50 @@ void command::PWMPinListener::HandleInterrupt()
 
 static void HandleInterrupt1()
 {
-    if (instances.size() > 0) {
-        instances[0]->HandleInterrupt();
-    }
+    instances.at(0)->HandleInterrupt();
 }
 
 static void HandleInterrupt2()
 {
-    if (instances.size() > 1) {
-        instances[1]->HandleInterrupt();
-    }
+    instances.at(1)->HandleInterrupt();
 }
 
 static void HandleInterrupt3()
 {
-    if (instances.size() > 2) {
-        instances[2]->HandleInterrupt();
-    }
+    instances.at(2)->HandleInterrupt();
 }
 
 static void HandleInterrupt4()
 {
-    if (instances.size() > 3) {
-        instances[3]->HandleInterrupt();
-    }
+    instances.at(3)->HandleInterrupt();
 }
 
 static void HandleInterrupt5()
 {
-    if (instances.size() > 4) {
-        instances[4]->HandleInterrupt();
-    }
+    instances.at(4)->HandleInterrupt();
 }
 
 static void HandleInterrupt6()
 {
-    if (instances.size() > 5) {
-        instances[5]->HandleInterrupt();
-    }
+    instances.at(5)->HandleInterrupt();
 }
 
 static void HandleInterrupt7()
 {
-    if (instances.size() > 6) {
-        instances[6]->HandleInterrupt();
-    }
+    instances.at(6)->HandleInterrupt();
 }
 
 static void HandleInterrupt8()
 {
-    if (instances.size() > 7) {
-        instances[7]->HandleInterrupt();
-    }
+    instances.at(7)->HandleInterrupt();
 }
 
 static void HandleInterrupt9()
 {
-    if (instances.size() > 8) {
-        instances[8]->HandleInterrupt();
-    }
+    instances.at(8)->HandleInterrupt();
 }
 
 static void HandleInterrupt10()
 {
-    if (instances.size() > 9) {
-        instances[9]->HandleInterrupt();
-    }
+    instances.at(9)->HandleInterrupt();
 }
